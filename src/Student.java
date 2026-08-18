cb07c4c545c0ce69c53830074b613a94d34b0e51
Sumera Naz <sumeranaz2338@gmail.com>
2026-08-18 16:32:13 +0500

    feat(student): add Student.addScore method

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    private List<Double> scores;
+
+    public Student() {
+        scores = new ArrayList<>();
+
+        public void addScore(double score) {
+    scores.add(score);
+}
+    }
+
+    }
